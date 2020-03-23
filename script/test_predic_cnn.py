bd3fd5d343320fe6ceab41c8b9e2f1729ed4a047
valabregue <romain.valabregue@upmc.fr>
2020-03-23 10:34:19 +0100

    some python testing

--- script/test_predic_cnn.py
+++ script/test_predic_cnn.py
@@ -3,6 +3,7 @@ from utils_file import gfile, gdir, get_parent_path
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import torch.optim as optim
 
 
 d='/home/romain.valabregue/QCcnn/li'
@@ -20,3 +21,35 @@ for ff in fres[4:-1] :
 ss=np.hstack(sujall)
 len(ss)
 len(np.unique(ss))
+
+
+f=gfile('/home/romain/QCcnn/li/','.*csv')
+for ff in f:
+    res=pd.read_csv(ff)
+    #plt.scatter(res.ssim,res.model_out)
+    #plt.plot(res.ssim, res.ssim,'k+')
+    err = np.abs(res.ssim-res.model_out) #same as L1 loss
+    plt.figure('err'); plt.plot(err)
+
+    errcum = np.cumsum(err[50:-1])/range(1,len(err)-51+1)
+    plt.figure('err_cum');        plt.plot(errcum)
+    N=50
+    err_slidin = np.convolve(err, np.ones((N,))/N, mode='valid')
+
+    plt.figure('err_slide'); plt.plot(err_slidin)
+plt.legend(('1','2','3','4','5'))
+
+import torch.nn as nn
+model = nn.Linear(10, 2)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+steps = 10
+scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, steps)
+scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.1, last_epoch=-1)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1, last_epoch=-1)
+for epoch in range(5):
+    for idx in range(steps):
+        scheduler.step()
+        print(scheduler.get_lr())
+
+    #print('Reset scheduler')
+    #scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, steps)
