c1f69a3286e4dabc51f0b7aa9642f1a88855c6f4
valabregue <romain.valabregue@upmc.fr>
2020-05-11 09:06:25 +0200

    more automatic choice of prepend transform

--- train_regress_motion_full.py
+++ train_regress_motion_full.py
@@ -1,11 +1,12 @@
 from doit_train import do_training, get_motion_transform, get_train_and_val_csv, get_cache_dir
 import torch
-from torchio.transforms import CropOrPad, RescaleIntensity, RandomAffine
+from torchio.transforms import CropOrPad, RescaleIntensity, RandomAffine, ApplyMask
 
 torch.multiprocessing.set_sharing_strategy('file_system')
 
 do_save, do_eval, test_sample = False, False, False
 make_uniform = False
+mask_brain = True
 batch_size, num_workers, max_epochs = 4, 0, 50
 cuda, verbose = True, True
 in_size = [182, 218, 182]
@@ -19,17 +20,18 @@ name_list_val = ['mask_mvt_val_cati_T1', 'mask_mvt_val_cati_ms', 'mask_mvt_val_c
 #name_list_val = ['mask_mvt_val_hcp200_ms', 'mask_mvt_val_hcp200_brain_ms', 'mask_mvt_val_hcp200_T1']
 
 name_list_train = [ 'ela1_train_cati_T1', 'ela1_train_cati_ms', 'ela1_train_cati_brain',
-                    'ela1_train_hcp400_ms', 'ela1_train_hcp400_brain_ms', 'ela1_train_hcp400_T1',
+                    'ela1_train_hcp400_ms', 'ela1_train_hcp400_T1',
                 'ela1_train200_hcp400_ms']
 name_list_val = ['ela1_val_cati_T1', 'ela1_val_cati_ms', 'ela1_val_cati_brain_ms',
-                 'ela1_val_hcp200_ms', 'ela1_val_hcp200_brain_ms', 'ela1_val_hcp200_T1',
+                 'ela1_val_hcp200_ms', 'ela1_val_hcp200_T1',
 		'ela1_train200_hcp400_ms']
 
-data_name_train = name_list_train[5]
-data_name_val = name_list_val[5]
+data_name_train = name_list_train[3]
+data_name_val = name_list_val[3]
 
 res_dir = '/network/lustre/iss01/cenir/analyse/irm/users/romain.valabregue/QCcnn/NN_regres_motion/'
 base_name = 'RegMotNew_resc_Aff'
+base_name = 'RegMotNew'
 if make_uniform : base_name += '_uniform'
 
 root_fs = 'le70'
@@ -44,11 +46,43 @@ par_model = {'network_name': 'ConvN',
              'cuda': cuda, 'max_epochs': max_epochs}
 #'conv_block':[8, 16, 32, 64, 128]
 
+tc=[]
+add_log = ''
+
+if mask_brain and 'hcp' in data_name_train:
+    add_to_load, add_to_load_regexp = 'brain', 'brain_T'
+else:
+    add_to_load, add_to_load_regexp = None, None
+
+if 'cati' in data_name_train:
+    target_shape, mask_key = (182, 218, 182), 'brain'
+    add_log += 'adding a CropOrPad {} with mask key {}'.format(target_shape, mask_key)
+    print(add_log)
+    tc.append( [CropOrPad(target_shape=target_shape, mask_name=mask_key), ] )
+
+# before RescaleIntensity for hcp le 07/04/2020 mais pas pour cati
+if mask_brain:
+    tc.append(ApplyMask(masking_method='brain'))
+    add_log += 'adding a ApplyMask brain '
+    base_name += '_Mask'
+
+if 'T1' in data_name_train:
+    tc.append(RescaleIntensity(percentiles=(0, 99)))
+    #tc.append(RandomAffine())
+    add_log += 'adding a RESCALE Intensity 0 99 '
+    base_name += '_rescale'
+    print(add_log)
+
+if len(tc) == 0: tc = None
+if len(add_log) == 0: add_log = None
+
+
 dir_cache = get_cache_dir(root_fs=root_fs)
 load_from_dir = ['{}/{}/'.format(dir_cache, data_name_train), '{}/{}/'.format(dir_cache, data_name_val)]
 res_name = '{}_{}'.format(base_name, data_name_train)
 
 doit = do_training(res_dir, res_name, verbose)
+
 
 if do_save:
     #rr test
@@ -80,25 +114,23 @@ elif test_sample:
         dd = data['image']['data'].reshape(-1).numpy()
         llog.info('{} max is {}'.format(i, np.max(dd)))
 
+    from plot_dataset import PlotDataset
+    PlotDataset(doit.train_dataset, image_key_name='image',subject_idx=20, subject_org=(5,4),
+                views=[['sag', 'vox', 50], ['cor', 'vox', 50]])
 else:
-    if 'cati' in data_name_train:
-        target_shape, mask_key = (182, 218, 182), 'brain'
-        print('adding a CropOrPad {} with mask key {}'.format(target_shape, mask_key))
-        tc = [CropOrPad(target_shape=target_shape, mask_name=mask_key), ]
-    else:
-        tc = None
-    if 'T1' in data_name_train:
-        if tc is None: tc=[]
-        tc.append(RescaleIntensity(percentiles=(0, 99)))
-        tc.append(RandomAffine())
-        print('adding a RESCALE Intensity 0 99 ')
-
 
     if make_uniform:
         doit.set_data_loader(batch_size=batch_size, num_workers=num_workers, load_from_dir=load_from_dir, transforms=tc,
-                             get_condition_csv='res_motion.csv', get_condition_field='ssim_brain' )
+                             get_condition_csv='res_motion.csv', get_condition_field='ssim_brain',
+                             add_to_load=add_to_load, add_to_load_regexp=add_to_load_regexp)
     else:
-        doit.set_data_loader(batch_size=batch_size, num_workers=num_workers, load_from_dir=load_from_dir, transforms=tc)
+        doit.set_data_loader(batch_size=batch_size, num_workers=num_workers, load_from_dir=load_from_dir, transforms=tc,
+                             add_to_load=add_to_load, add_to_load_regexp=add_to_load_regexp)
     doit.set_model(par_model)
+
+    if add_log is not None:
+        llog = doit.log
+        llog.info(add_log)
+
     doit.train_regress_motion()
 
