58749c1fc71d1a03e91fe59e863309b8ea774e52
GFabien <fabien.girka@supelec.fr>
2020-07-02 11:15:02 +0200

    Handle background in target for multi label masks

--- segmentation/metrics/utils.py
+++ segmentation/metrics/utils.py
@@ -6,8 +6,8 @@ def mean_metric(prediction, target, metric, channels=None, mask=None):
     """
     Compute a given metric on every channel of the volumes and average them.
     """
-    if target.shape[1] == 1:
-        target = torch.cat([target, 1 - target], dim=1)
+    if (prediction.shape[1] - target.shape[1]) == 1:
+        target = torch.cat([target, 1 - target.sum(dim=1, keepdim=True)], dim=1)
     prediction = F.softmax(prediction, dim=1)
 
     if channels is not None:
