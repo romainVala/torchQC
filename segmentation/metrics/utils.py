eb872900239b69081b4aa3fff031e01b9e9f9f17
GFabien <fabien.girka@supelec.fr>
2020-07-22 11:04:00 +0200

    Add option to calculate dice loss with argmax-binarized volume

--- segmentation/metrics/utils.py
+++ segmentation/metrics/utils.py
@@ -2,13 +2,18 @@ import torch
 import torch.nn.functional as F
 
 
-def mean_metric(prediction, target, metric, channels=None, mask=None):
+def mean_metric(prediction, target, metric, channels=None, mask=None, binary=False):
     """
     Compute a given metric on every channel of the volumes and average them.
     """
     if (prediction.shape[1] - target.shape[1]) == 1:
         target = torch.cat([target, 1 - target.sum(dim=1, keepdim=True)], dim=1)
     prediction = F.softmax(prediction, dim=1)
+
+    if binary:
+        indices = torch.argmax(target, dim=1)
+        target = F.one_hot(indices)
+        target = target.permute(0, 4, 1, 2, 3).float()
 
     if channels is not None:
         prediction = prediction[:, channels, ...]
