e53ca0d4504349b7a4b310b0c663c83a284b1bbb
GFabien <fabien.girka@supelec.fr>
2020-08-31 15:18:33 +0200

    Refactor metric handling using overlay_metric function
    * Import activation using module, package and name
    * Can specify activation for metrics
    * Encapsulate any metric call to overlay_metric

--- segmentation/losses/dice_loss.py
+++ segmentation/losses/dice_loss.py
@@ -6,34 +6,28 @@ class Dice:
     Implements different variants of the Dice Loss.
 
     Args:
-        cut: the threshold to binarize the volumes, default is None: volumes are not binarized.
+        cut: the threshold to binarize the volumes, default is None: volumes are
+            not binarized.
         smooth: a value used to avoid division by zero in soft dice loss.
     """
-    def __init__(self, cut=0.5, smooth=1., mask_cut=0.99, binary=False):
+    def __init__(self, cut=0.5, smooth=1., binary=False):
         self.cut = cut
         self.smooth = smooth
-        self.mask_cut = mask_cut
         self.binary = binary
 
-    def _apply_mask(self, prediction, target, mask):
-        if mask is None:
-            return prediction, target
-        mask = mask >= self.mask_cut
-        return prediction * mask, target * mask
-
-    def dice_loss(self, prediction, target, mask=None):
+    def dice_loss(self, prediction, target):
         target = target.float()
         prediction = prediction.float()
-        prediction, target = self._apply_mask(prediction, target, mask)
         input_flat = prediction.contiguous().view(-1)
         target_flat = target.contiguous().view(-1)
         intersection = (input_flat * target_flat).sum()
         return 1 - ((2. * intersection + self.smooth) /
-                    (input_flat.pow(2).sum() + target_flat.pow(2).sum() + self.smooth))
+                    (input_flat.pow(2).sum() + target_flat.pow(2).sum()
+                     + self.smooth))
 
-    def mean_dice_loss(self, prediction, target, **kwargs):
-        return mean_metric(prediction, target, self.dice_loss, binary=self.binary, **kwargs)
+    def mean_dice_loss(self, prediction, target):
+        return mean_metric(prediction, target, self.dice_loss)
 
-    def mean_binarized_dice_loss(self, prediction, target, **kwargs):
+    def mean_binarized_dice_loss(self, prediction, target):
         target = (target > self.cut).float()
-        return self.mean_dice_loss(prediction, target, **kwargs)
+        return self.mean_dice_loss(prediction, target)
--- segmentation/metrics/fuzzy_overlap_metrics.py
+++ segmentation/metrics/fuzzy_overlap_metrics.py
@@ -20,45 +20,44 @@ def product_t_norm(prediction, target, background=False):
 
 class FuzzyOverlapMetric:
     """
-        Implements the different fuzzy overlap based metrics (TP, TN, FP, FN) using a given t_norm.
+        Implements the different fuzzy overlap based metrics (TP, TN, FP, FN)
+        using a given t_norm.
 
         Args:
-            t_norm: the t_norm to use to compute the agreement between the volumes.
+            t_norm: the t_norm to use to compute the agreement between the
+                volumes.
         """
-    def __init__(self, t_norm=minimum_t_norm, mask_cut=0.99):
+    def __init__(self, t_norm=minimum_t_norm):
         self.t_norm = t_norm
-        self.mask_cut = mask_cut
 
-    def _apply_mask(self, mapping, mask):
-        if mask is None:
-            return mapping
-        mask = mask >= self.mask_cut
-        return mapping * mask
+    def fuzzy_true_positives_map(self, prediction, target):
+        return self.t_norm(prediction, target, background=False)
 
-    def fuzzy_true_positives_map(self, prediction, target, mask=None):
-        mapping = self.t_norm(prediction, target, background=False)
-        return self._apply_mask(mapping, mask)
+    def fuzzy_true_negatives_map(self, prediction, target):
+        return self.t_norm(1 - prediction, 1 - target, background=False)
 
-    def fuzzy_true_negatives_map(self, prediction, target, mask=None):
-        mapping = self.t_norm(1 - prediction, 1 - target, background=False)
-        return self._apply_mask(mapping, mask)
+    def fuzzy_false_positives_map(self, prediction, target):
+        return self.t_norm(prediction, target, background=True)
 
-    def fuzzy_false_positives_map(self, prediction, target, mask=None):
-        mapping = self.t_norm(prediction, target, background=True)
-        return self._apply_mask(mapping, mask)
+    def fuzzy_false_negatives_map(self, prediction, target):
+        return self.t_norm(1 - prediction, 1 - target, background=True)
 
-    def fuzzy_false_negatives_map(self, prediction, target, mask=None):
-        mapping = self.t_norm(1 - prediction, 1 - target, background=True)
-        return self._apply_mask(mapping, mask)
+    def mean_fuzzy_true_positives(self, prediction, target):
+        return mean_metric(
+            prediction, target, lambda p, t: self.fuzzy_true_positives_map(p, t).sum()
+        )
 
-    def mean_fuzzy_true_positives(self, prediction, target, **kwargs):
-        return mean_metric(prediction, target, lambda p, t, m: self.fuzzy_true_positives_map(p, t, m).sum(), **kwargs)
+    def mean_fuzzy_true_negatives(self, prediction, target):
+        return mean_metric(
+            prediction, target, lambda p, t: self.fuzzy_true_negatives_map(p, t).sum()
+        )
 
-    def mean_fuzzy_true_negatives(self, prediction, target, **kwargs):
-        return mean_metric(prediction, target, lambda p, t, m: self.fuzzy_true_negatives_map(p, t, m).sum(), **kwargs)
+    def mean_fuzzy_false_positives(self, prediction, target):
+        return mean_metric(
+            prediction, target, lambda p, t: self.fuzzy_false_positives_map(p, t).sum()
+        )
 
-    def mean_fuzzy_false_positives(self, prediction, target, **kwargs):
-        return mean_metric(prediction, target, lambda p, t, m: self.fuzzy_false_positives_map(p, t, m).sum(), **kwargs)
-
-    def mean_fuzzy_false_negatives(self, prediction, target, **kwargs):
-        return mean_metric(prediction, target, lambda p, t, m: self.fuzzy_false_negatives_map(p, t, m).sum(), **kwargs)
+    def mean_fuzzy_false_negatives(self, prediction, target):
+        return mean_metric(
+            prediction, target, lambda p, t: self.fuzzy_false_negatives_map(p, t).sum()
+        )
