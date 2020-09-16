2c036f5e2a6bb47f77258447191a5e3b0085b4da
GFabien <fabien.girka@supelec.fr>
2020-09-16 17:27:53 +0200

    Enable to binarize predictions in MetricOverlay and add volume ratio metric

--- segmentation/metrics/utils.py
+++ segmentation/metrics/utils.py
@@ -4,14 +4,29 @@ import torch.nn.functional as F
 
 class MetricOverlay:
     def __init__(self, metric, channels=None, mask=None, mask_cut=(0.99, 1),
-                 binary=False, activation=None, binary_volumes=False):
+                 binarize_target=False, activation=None, binary_volumes=False,
+                 binarize_prediction=False):
         self.metric = metric
         self.channels = channels
         self.mask = mask
         self.mask_cut = mask_cut
-        self.binary = binary
+        self.binarize_target = binarize_target
         self.activation = activation
         self.binary_volumes = binary_volumes
+        self.binarize_prediction = binarize_prediction
+
+    @staticmethod
+    def binarize(tensor):
+        unlabeled_volume = tensor.sum(dim=1) < 0.5
+        val, indices = torch.max(tensor, dim=1)
+        extra_class = indices.max() + 1
+        indices[val == 0] = extra_class
+        indices[unlabeled_volume] = extra_class
+        tensor = F.one_hot(indices)
+        if (val == 0).sum() or unlabeled_volume.sum():
+            tensor = tensor[..., :-1]
+        tensor = tensor.permute(0, 4, 1, 2, 3).float()
+        return tensor
 
     def __call__(self, prediction, target):
         if (prediction.shape[1] - target.shape[1]) == 1:
@@ -27,16 +42,11 @@ class MetricOverlay:
         if self.activation is not None:
             prediction = self.activation(prediction)
 
-        if self.binary:
-            unlabeled_volume = target.sum(dim=1) < 0.5
-            val, indices = torch.max(target, dim=1)
-            extra_class = indices.max() + 1
-            indices[val == 0] = extra_class
-            indices[unlabeled_volume] = extra_class
-            target = F.one_hot(indices)
-            if (val == 0).sum() or unlabeled_volume.sum():
-                target = target[..., :-1]
-            target = target.permute(0, 4, 1, 2, 3).float()
+        if self.binarize_target:
+            target = self.binarize(target)
+
+        if self.binarize_prediction:
+            prediction = self.binarize_prediction
 
         if self.mask is not None:
             min_cut, max_cut = self.mask_cut
