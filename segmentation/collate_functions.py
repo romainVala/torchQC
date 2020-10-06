9f2802a96bdca306c40da2b6fc8d6cd22c34b698
GReguig <ghiles.reguig@gmail.com>
2020-10-06 18:44:05 +0200

    adding a collate function

--- segmentation/collate_functions.py
+++ segmentation/collate_functions.py
@@ -48,12 +48,65 @@ def history_collate(batch):
         # The only change to the original function is here:
         # if elem has attribute 'history', then a key 'history' is added to the batch
         # which value is the list of the history of the elements of the batch
+        dictionary = {key: history_collate([d[key] for d in batch]) for key in elem}
+        if hasattr(elem, 'history'):
+            dictionary.update({
+                'history': [d.history for d in batch]
+            })
+        return dictionary
+    elif isinstance(elem, tuple) and hasattr(elem, '_fields'):  # namedtuple
+        return elem_type(*(history_collate(samples) for samples in zip(*batch)))
+    elif isinstance(elem, container_abcs.Sequence):
+        # check to make sure that the elements in batch have consistent size
+        it = iter(batch)
+        elem_size = len(next(it))
+        if not all(len(elem) == elem_size for elem in it):
+            raise RuntimeError('each element in list of batch should be of equal size')
+        transposed = zip(*batch)
+        return [history_collate(samples) for samples in transposed]
+
+    raise TypeError(default_collate_err_msg_format.format(elem_type))
+
+
+def history_collate_partial_metrics(batch):
+    """
+    Adapt default_collate from https://github.com/pytorch/pytorch/blob/master/torch/utils/data/_utils/collate.py
+    to handle random transform history in batches.
+    """
+
+    elem = batch[0]
+    elem_type = type(elem)
+    if isinstance(elem, torch.Tensor):
+        out = None
+        if torch.utils.data.get_worker_info() is not None:
+            # If we're in a background process, concatenate directly into a
+            # shared memory tensor to avoid an extra copy
+            numel = sum([x.numel() for x in batch])
+            storage = elem.storage()._new_shared(numel)
+            out = elem.new(storage)
+        return torch.stack(batch, 0, out=out)
+    elif elem_type.__module__ == 'numpy' and elem_type.__name__ != 'str_' \
+            and elem_type.__name__ != 'string_':
+        elem = batch[0]
+        if elem_type.__name__ == 'ndarray':
+            # array of string classes and object
+            if np_str_obj_array_pattern.search(elem.dtype.str) is not None:
+                raise TypeError(default_collate_err_msg_format.format(elem.dtype))
+
+            return history_collate([torch.as_tensor(b) for b in batch])
+        elif elem.shape == ():  # scalars
+            return torch.as_tensor(batch)
+    elif isinstance(elem, float):
+        return torch.tensor(batch, dtype=torch.float64)
+    elif isinstance(elem, int_classes):
+        return torch.tensor(batch)
+    elif isinstance(elem, string_classes):
+        return batch
+    elif isinstance(elem, container_abcs.Mapping):
+        # The only change to the original function is here:
+        # if elem has attribute 'history', then a key 'history' is added to the batch
+        # which value is the list of the history of the elements of the batch
         batch_keys = set.union(*[set(d.keys()) for d in batch])
-        """
-        dictionary = {key: history_collate([d[key] if key in d.keys() else torch.Tensor([[np.nan]*len(batch)])
-                                            for d in batch])
-                      for key in elem}
-        """
         dictionary = {}
         metrics_idx = []
         metrics_values = []
@@ -75,7 +128,6 @@ def history_collate(batch):
             if len(metrics_idx) != len(batch):
                 res_metrics = np.asarray([dict()]*4)
                 res_metrics[metrics_idx] = metrics_values
-            print("res_metrics: {}".format(res_metrics))
             dictionary.update({"metrics": res_metrics})
 
         if hasattr(elem, 'history'):
