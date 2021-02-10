b677be53ba1a3d1ba09ed167e309fda473d85ae8
valabregue <romain.valabregue@upmc.fr>
2021-02-10 18:07:15 +0100

    working

--- script/test_motion_on_synth2.py
+++ script/test_motion_on_synth2.py
@@ -12,13 +12,52 @@ from types import SimpleNamespace
 from kymatio import HarmonicScattering3D
 
 def show_df(df, keys):
-    rout = dict()
+    rout, rout_unique = dict(),dict()
     if isinstance(keys,dict):
         keys = keys.keys()
     for k in keys:
-        print(f'  {k} : {df[k].unique()}')
-        rout[k] = df[k].unique()
-    return rout
+        vars = df[k].unique()
+        if len(vars)==1:
+            rout_unique[k] = vars
+        else:
+            vars = np.sort(vars)
+            rout[k] = vars
+    print('unique values')
+    for k, v in rout_unique.items():
+        print(f'  {k} \t: {v}')
+    print('multiple values')
+    for k, v in rout.items():
+        print(f'  {k} \t: {v}')
+
+    #return rout, rout_unique
+def correct_string_bool(x):
+    if x=='0.0':
+        x = 'False'
+    if x=='1.0':
+        x = 'True'
+    return x
+def split_df_unique_vals(df, list_keys):
+    dfout = dict()
+    k = list_keys[0]
+    vals = df[k].unique()
+    for vv in vals:
+        out_name = f'{k}_{vv}'
+        dfsub = df[df[k]==vv]
+        dfout[out_name] = dfsub
+
+    if len(list_keys)>1:
+        list_keys = list_keys[1:]
+        for k_filter in list_keys:
+            New_dfout = dict()
+            for k, v in dfout.items():
+                dd=split_df_unique_vals(v, [k_filter])
+                for kk, vv in dd.items():
+                    out_name = f'{k}_{kk}'
+                    New_dfout[out_name] = vv
+            dfout = New_dfout
+
+    return dfout
+
 
 #not working, too bad ... from torchio.transforms.augmentation.intensity.torch_random_motion import TorchRandomMotionFromTimeCourse
 #volume=tt.permute(1,2,3,0).numpy()
@@ -68,9 +107,11 @@ all_params = dict(
     cor_disp = [True,],
     disp_str =  ['no_shift'],
     suj_index = [ 475,  478, 492, 497, 499, 500], #[474, 475, 477, 478, 485, 492, 497, 499, 500, 510],
-    suj_seed = [0,1,2,4,5,6,7,8,9],
+    suj_seed = [0,2,4,7,9], #[0,1,2,4,5,6,7,8,9],
     suj_contrast = [1, 2,3],
-    suj_deform = [False, True,]
+    suj_deform = [False, True,],
+    suj_nois = [0.01, 0.1]
+
 )
 all_params['suj_index'] = [0]
 
@@ -89,11 +130,11 @@ resolution=512
 
 file = '/network/lustre/iss01/cenir/analyse/irm/users/romain.valabregue/PVsynth/job/motion/test1/main.json'
 res_name = 'transX_ustep_sym_nosym';res_name = 'transX_ustep_sym_10suj'
-res_name = 'transX_ustep_sym_5suj_10seed_3contrast_2deform'
 res_name = 'transX_ustep_65X0_5suj_10seed_3contrast_2deform'
+res_name = 'noise_transX_ustep_65X0_1suj_5seed_3contrast_2deform'
 out_path = '/network/lustre/iss01/cenir/analyse/irm/users/romain.valabregue/PVsynth/job/motion/' + res_name
 
-create_motion_job(params,4,file,out_path,res_name)
+create_motion_job(params,1,file,out_path,res_name)
 
 mres = ModelCSVResults(df_data=df1, out_tmp="/tmp/rrr")
 keys_unpack = ['T_LabelsToImage'];
@@ -103,27 +144,47 @@ df1 = mres.normalize_dict_to_df(keys_unpack, suffix=suffix);
 csv_file = glob.glob(out_path+'/res_metrics*')
 df = [pd.read_csv(ff) for ff in csv_file]
 df1 = pd.concat(df, ignore_index=True); dfall = df1
+show_df(df1,all_params)
+
+df1['m_wTF_Disp_1']=df1['m_wTF_Disp_1'] + df1['shift']
+df1['m_wTF2_Disp_1']=df1['m_wTF2_Disp_1'] + df1['shift']
+
 df1 = dfall[dfall['mvt_type']=='Ustep_sym']
 df1 = df1[df1['xend']==256]
 
 dfsub = df1[df1.mvt_axe=='transY'] ; # df1[df1.mvt_axe=='transX'] ;
 dfsub = dfsub[dfsub.mvt_type=='Ustep'] #dfsub[dfsub.mvt_type=='Ustep_sym']
-dfsub = df1[df1.suj_contrast==1]; dfsub = dfsub[dfsub.suj_deform==0];
-ds1 = dfsub[dfsub.sigma==64] ;ds1 = ds1[ds1.x0==212] ; ds1= ds1[ds1.amplitude==4] ; ds1.shape
+dfsub = df1[df1.suj_contrast==3]; dfsub = dfsub[dfsub.suj_deform==1];
+dfsub = df1[ (df1.amplitude==4) & (df1.suj_contrast==1)];
+ds1 = dfsub[(dfsub.sigma==64) & (dfsub.x0==256) & (dfsub.suj_deform==1)]; ds1.shape
+dfsub_dict = split_df_unique_vals(df1,['amplitude'])
+dfsub = split_df_unique_vals(df1,['suj_seed'])
 
+ykeys=['m_t1_L1_map', 'm_t1_NCC', 'm_t1_ssim_SSIM', 'm_t1_PSNR','m_t1_grad_ratio','m_t1_lab_std0','m_t1_lab_mean0',
+         'm_t1_nRMSE', 'm_t1_grad_nMI2', 'm_t1_grad_Eratio','m_t1_grad_EGratio','m_t1_grad_ratio']
+#ykeys = ['m_t1_PSNR', 'm_t1_grad_ratio']  # ,'m_t1_nRMSE'] #,'m_t1_L1_map'] m_t1_grad_grad_ration
+'mean_DispP', 'm_meanDispP_wTF', 'm_meanDispP_wTF2', 'm_wTF_Disp_1', 'm_wTF2_Disp_1',
 
-ykeys =[ 'm_t1_PSNR' ,'m_t1_grad_ratio' ,'m_t1_nRMSE'] #,'m_t1_L1_map'] m_t1_grad_grad_ration
-cmap = sns.color_palette("coolwarm", len(df1.sigma.unique()))
-for ykey in ykeys:
-    fig = sns.relplot(data=dfsub, x="xend", y=ykey, hue="sigma", legend='full', kind="line",palette=cmap, col='amplitude',col_wrap=2)
-    for aa in fig.axes: aa.grid()
+figres = '/network/lustre/iss01/cenir/analyse/irm/users/romain.valabregue/PVsynth/job/motion/figure/'
+prefix = 'contrast_'
+for k, dfsub in dfsub_dict.items():
+    cmap = sns.color_palette("coolwarm", len(dfsub.sigma.unique()))
+    for ykey in ykeys:
+        #fig = sns.relplot(data=dfsub, x="xend", y=ykey, hue="sigma", legend='full', kind="line",palette=cmap, col='amplitude',col_wrap=2)
+        fig = sns.relplot(data=dfsub, x="xend", y=ykey, hue="sigma", legend='full', kind="line",palette=cmap, col='suj_contrast',col_wrap=2,style='suj_deform')
+        for aa in fig.axes: aa.grid()
+        plt.text(0.8,0.3,ykey,transform=plt.gcf().transFigure,bbox={'facecolor': 'grey', 'alpha': 0.5, 'pad': 10})
+        plt.text(0.8,0.2,k,transform=plt.gcf().transFigure,bbox={'facecolor': 'grey', 'alpha': 0.5, 'pad': 10})
+        figname = f'{prefix}_{ykey}_{k}.png'
+        fig.savefig(figres+figname)
+        plt.close()
 
 for suj in df1['sujname'].unique():
     dfsub = df1[ df1['sujname'] == suj ]
     fig = sns.relplot(data=dfsub, x="xend", y=ykey, hue="sigma", legend='full', kind="line",palette=cmap, col='amplitude',col_wrap=2)
     for aa in fig.axes: aa.grid()
 
-fig = sns.relplot(data=dfsub, x="xend", y=ykey, hue="sigma", legend='full', kind="line",palette=cmap, col='amplitude',col_wrap=2, style='sujname')
+fig = sns.relplot(data=dfsub, x="xend", y=ykey, hue="sigma", legend='full', kind="line",palette=cmap, col='suj_seed',col_wrap=3, style='suj_deform')
 for aa in fig.axes: aa.grid()
 
 cmap = sns.color_palette("coolwarm", len(df1.suj_seed.unique()))
@@ -149,11 +210,21 @@ fig=sns.relplot(data=df1, x="shift", y="m_wTF_Disp_1", hue="sigma", legend='full
 for aa in fig.axes:
     aa.plot([0,10], [0,10])
 
-
+sel_key=['m_wTF_Disp_1', 'm_wTF2_Disp_1', 'shift']
 sel_key=['m_t1_L1_map', 'm_t1_NCC', 'm_t1_ssim_SSIM', 'm_t1_PSNR','m_t1_grad_ratio','m_t1_lab_std0',
          'm_meanDispP_wTF2','m_rmse_Disp_wTF2']
+sel_key=['m_t1_PSNR', 'm_t1_NCC', 'm_t1_grad_Eratio','m_t1_grad_EGratio','m_t1_grad_ratio']
 sel_key=['m_t1_L1_map', 'm_t1_NCC', 'm_t1_ssim_SSIM', 'm_t1_PSNR','m_t1_grad_ratio','m_t1_lab_std0','m_t1_nRMSE']
-sns.pairplot(df1[sel_key], kind="scatter", corner=True)
+fig = sns.pairplot(df1[sel_key], kind="scatter", corner=True)
+
+cmap = sns.color_palette("coolwarm", len(df1.sigma.unique()))
+fig = sns.relplot(data=dfsub, x="shift", y='m_wTF_Disp_1', hue="sigma", legend='full', kind="scatter",palette=cmap, col='amplitude',col_wrap=2,style='suj_seed')
+cmap = sns.color_palette("coolwarm", len(df1.amplitude.unique()))
+fig = sns.relplot(data=dfsub, x="shift", y='m_wTF_Disp_1', hue="amplitude", legend='full', kind="scatter",palette=cmap,
+                  col='sigma',col_wrap=2,style='suj_seed')
+for aa in fig.axes:
+    aa.plot([0,8], [0,8]); aa.grid()
+
 
 d= result_dir + '/'
 f1= d+ 'Train_ep001.csv'
@@ -264,7 +335,45 @@ from scipy.signal import fftconvolve
 dd = data[::-1,::-1,::-1]
 rr = fftconvolve(data,data)
 
-#retrieve one suj
+############## CLUSTERING
+
+ykeys=['m_t1_L1_map', 'm_t1_NCC', 'm_t1_ssim_SSIM', 'm_t1_PSNR','m_t1_grad_ratio','m_t1_nRMSE','m_t1_grad_Eratio','m_t1_grad_EGratio','m_t1_grad_ratio']
+dsub = df1.loc[:,ykeys]
+data = dsub.to_numpy()
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import StandardScaler
+ # A list holds the SSE values for each k
+sse = [];silhouette_coefficients = []
+kmeans_kwargs = {"init": "random","n_init": 10,"max_iter": 300,"random_state": 42 }
+for k in range(2, 11):
+    kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
+    kmeans.fit(data)
+    sse.append(kmeans.inertia_)
+    score = silhouette_score(data, kmeans.labels_)
+    silhouette_coefficients.append(score)
+plt.figure();plt.plot(sse)
+plt.figure();plt.plot(silhouette_coefficients)
+
+from sklearn.decomposition import PCA, KernelPCA
+kpca = KernelPCA(kernel="rbf", fit_inverse_transform=True, gamma=10)
+X_kpca = kpca.fit_transform(data)
+X_back = kpca.inverse_transform(X_kpca)
+pca = PCA()
+X_pca = pca.fit_transform(data)
+
+from sklearn import manifold
+tsne = manifold.TSNE(n_components=3, init='random',random_state=0, perplexity=50)
+Y = tsne.fit_transform(data)
+
+#and so what ???
+
+
+
+
+
+##############retrieve one suj
 from util_affine import select_data, corrupt_data, apply_motion
 from types import SimpleNamespace
 
@@ -305,3 +414,34 @@ for i,pp in enumerate(params):
     if all_equal:
         print(i)
 
+
+############################ contrast
+label_list = ['GM', 'CSF', 'WM',  'L_Accu','L_Amyg', 'L_Caud', 'L_Hipp', 'L_Pall', 'L_Puta', 'L_Thal',  'BrStem', 'cereb_GM','cereb_WM', 'skin',  'skull', 'background']
+PV = ssynth.label.data
+pv_lin = PV.flatten(start_dim=1)
+pv_inter = torch.matmul(pv_lin,pv_lin.transpose(1,0))
+pv_inter_norm = torch.zeros_like(pv_inter)
+pv_inter[0]
+for i, l in enumerate(label_list):
+    #print('{} vol {} {}'.format(l, torch.sum(PV[i]), torch.sum(pv_inter[i])))
+    inter_vol = pv_inter[i] / torch.sum(pv_inter[i]) * 100
+    inter_vol[inter_vol < 0.01] = 0
+    pv_inter_norm[i] = inter_vol; pv_inter_norm[i,i] = 0
+    ind_pv = torch.where(inter_vol>0)[0].numpy()
+    strr=f'{l}\t '
+    for found_ind, ii in enumerate(ind_pv):
+        strr += f' {label_list[ii]}  {inter_vol[ii]}'
+    print(strr)
+
+
+############## autocorrelation
+data=ssynth.t1.data[0]
+datam = smot.t1.data[0]
+
+c1,c2,c3,cdiff = get_autocor(data,nb_off_center = 5)
+c1m,c2m,c3m,cdiffm = get_autocor(datam,nb_off_center = 3)
+c1/c1m, c2/c2m, c3/c3m, cdiffm/cdiff
+
+plt.figure(); plt.plot(x_dis, y_cor); plt.plot(x_dism, y_corm);
+x=unique_dist
+plt.plot(x, m*x + b)
