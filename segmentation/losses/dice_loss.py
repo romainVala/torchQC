6bc94a8577de01e97755ede09bc2c96805d7d5fa
valabregue <romain.valabregue@upmc.fr>
2021-07-08 15:29:45 +0200

    remove binary attribute for Dice, since it was not used !

--- segmentation/losses/dice_loss.py
+++ segmentation/losses/dice_loss.py
@@ -59,10 +59,9 @@ class Dice:
             not binarized.
         smooth: a value used to avoid division by zero in soft dice loss.
     """
-    def __init__(self, cut=0.5, smooth=1., binary=False):
+    def __init__(self, cut=0.5, smooth=1.):
         self.cut = cut
         self.smooth = smooth
-        self.binary = binary
 
     def dice_loss(self, prediction, target):
         target = target.float()
