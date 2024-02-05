c2c32e7e65def70535e600d35554329ab623e2f0
valabregue <romain.valabregue@upmc.fr>
2024-02-05 14:54:04 +0100

    keep only model multiple of 10

---
+++ script/remove_model_weight.py
@@ -0,0 +1,35 @@
+import pandas as pd, glob, os
+from utils_file import get_all_recursif_dir, gfile
+from natsort import natsorted
+
+def delete_file_list(ff):
+    for file in ff:
+        os.remove(file)
+
+d = get_all_recursif_dir(os.getcwd())
+
+for dirs in d:
+    f = gfile(dirs, 'opt_ep.*tar')
+    delete_file_list(f)
+    f = gfile(dirs, 'amp_ep.*tar')
+    delete_file_list(f)
+
+    f = gfile(dirs, 'model_ep')
+    f = natsorted(f)
+    if len(f)==0:
+        print(f'empty dir {dirs}')
+        continue
+
+    f.pop(-1) #always keep last one
+    print(f'working in {dirs}')
+
+    for ff in f:
+        fname = os.path.basename(ff)
+        res = [i for i in range(len(fname)) if fname.startswith('_', i)]
+        fname[res[1]]
+        number = int(fname[res[1]-1])
+        if number!=0:
+            print(f'deleting {ff}')
+            os.remove(ff)
+        #else:
+        #    print(f'keeping {fname}')
